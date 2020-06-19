e3f7b067dfbddbbb40fbc78f425f92462beff9ae
David Rua Jaramillo <DavidRJ@interactuar.org.co>
2020-06-18 21:51:38 -0500

    This notebook will be mainly used for the capstone project

---
+++ coursera_code.py
@@ -0,0 +1,7 @@
+#This is the repository for Coursera Capstone in Data Science 
+#This notebook will be used for the capstone project and learning how to use it.
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course!')
+
